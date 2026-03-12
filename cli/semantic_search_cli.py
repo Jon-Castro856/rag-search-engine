2a0a859dcd854f9080f1f44797131c57301af1f6
Jon Castro <jonattancast@gmail.com>
2026-03-12 12:19:43 -0400

    beginning implementation of semantic search

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 import argparse
-from sentence_transformers import SentenceTransformer
+from lib.semantic_search import SemanticSearch, verify_model
 
 def main():
+    
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    verify = subparsers.add_parser("verify", help="verify creation of semantic search model")
     args = parser.parse_args()
+    
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
